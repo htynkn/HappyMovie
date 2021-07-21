e3a0d55be88ff0eb281c8f2f522bee7557dc7d43
htynkn <htynkn@gmail.com>
2021-07-21 19:13:02 +0800

    restructure

---
+++ Jellyfin.Plugin.HappyMovie/Configuration/PluginConfiguration.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+
+namespace Jellyfin.Plugin.HappyMovie.Configuration
+{
+    public class PluginConfiguration : MediaBrowser.Model.Plugins.BasePluginConfiguration
+    {
+        public string ApiKey { get; set; } = "4219e299c89411838049ab0dab19ebd5";
+    }
+}
